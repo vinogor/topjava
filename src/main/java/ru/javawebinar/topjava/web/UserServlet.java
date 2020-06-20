5ff92cc280146e64580905ebbe3761e9f87ea63b
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2020-06-20 15:44:33 +0300

    1 4 forward to redirect

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -6,10 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// дебаг в браузере Ctrl + Shift + I, вкладка Network
+// http://localhost:8080/topjava/
 public class UserServlet extends HttpServlet {
 
-    @Override
+    @Override // request.getParameterMap() - пары ключ-значение запросов из адресной строки
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/users.jsp").forward(request, response);
+//        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("users.jsp"); // запрос идёт через браузер
     }
 }
