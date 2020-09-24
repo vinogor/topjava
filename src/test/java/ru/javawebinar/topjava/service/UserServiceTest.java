920d653947ac15f3e1bb95c7cf79a446428f93bb
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2020-09-24 08:59:23 +0300

    4 0 fix

--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -11,12 +11,10 @@ import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.UserRepository;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import java.util.List;
 
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.UserTestData.*;
 
@@ -38,9 +36,6 @@ public class UserServiceTest {
     @Autowired
     private UserService service;
 
-    @Autowired
-    private UserRepository repository;
-
     @Test
     public void create() throws Exception {
         User newUser = getNew();
@@ -60,7 +55,7 @@ public class UserServiceTest {
     @Test
     public void delete() throws Exception {
         service.delete(USER_ID);
-        assertNull(repository.get(USER_ID));
+        assertThrows(NotFoundException.class, () -> service.get(USER_ID));
     }
 
     @Test
