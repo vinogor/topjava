51025fe79f33c09a5131ffd1735b9ed2aa58f43c
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2020-12-07 08:38:36 +0300

    6 14 spring webmvc

---
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -0,0 +1,38 @@
+package ru.javawebinar.topjava.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.javawebinar.topjava.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class RootController {
+
+    // прилетает из корневого контекста
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/")
+    public String root() {
+        // возвращаем имя вьюхи
+        return "index";
+    }
+
+    @GetMapping("/users")
+    public String getUsers(Model model) {
+        // заполняем инфой отрибут, который заюзаем в соответствующей вьюхе через ${users}
+        model.addAttribute("users", service.getAll());
+        return "users";
+    }
+
+    @PostMapping("/users")
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        SecurityUtil.setAuthUserId(userId);
+        return "redirect:meals";
+    }
+}
