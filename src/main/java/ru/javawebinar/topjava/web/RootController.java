09405da5444b8073cfd73c34e0de5cdf5fc3a9d6
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2020-12-29 08:41:11 +0300

    7 02 HW6 meals

--- src/main/java/ru/javawebinar/topjava/web/RootController.java
+++ src/main/java/ru/javawebinar/topjava/web/RootController.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.javawebinar.topjava.service.MealService;
 import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -14,7 +16,10 @@ public class RootController {
 
     // прилетает из корневого контекста
     @Autowired
-    private UserService service;
+    private UserService userService;
+
+    @Autowired
+    private MealService mealService;
 
     @GetMapping("/")
     public String root() {
@@ -25,7 +30,7 @@ public class RootController {
     @GetMapping("/users")
     public String getUsers(Model model) {
         // заполняем инфой отрибут, который заюзаем в соответствующей вьюхе через ${users}
-        model.addAttribute("users", service.getAll());
+        model.addAttribute("users", userService.getAll());
         return "users";
     }
 
@@ -34,5 +39,12 @@ public class RootController {
         int userId = Integer.parseInt(request.getParameter("userId"));
         SecurityUtil.setAuthUserId(userId);
         return "redirect:meals";
+    }
+
+    @GetMapping("/meals")
+    public String getMeals(Model model) {
+        model.addAttribute("meals",
+                MealsUtil.getTos(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
+        return "meals";
     }
 }
