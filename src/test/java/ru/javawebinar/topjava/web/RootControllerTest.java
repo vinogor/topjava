f5e90844470fa1aa344482b86968940d17baf86e
Andreev Aleksandr <andreev.aleksandr.spb@gmail.com>
2021-02-25 07:35:28 +0300

    HW7 RootControllerTest#mealsJSP w/o matcher, replace readme.md

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.web;
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.to.MealTo;
 
 import java.util.List;
 
@@ -11,7 +10,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.javawebinar.topjava.MealTestData.MEALS_TO;
-import static ru.javawebinar.topjava.MealTestData.MEAL_TO_MATCHER;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
@@ -40,14 +38,7 @@ class RootControllerTest extends AbstractControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("meals"))
                 .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
-                .andExpect(model().attribute("meals",
-                        new AssertionMatcher<List<MealTo>>() {
-                            @Override
-                            public void assertion(List<MealTo> actualTos) throws AssertionError {
-                                MEAL_TO_MATCHER.assertMatch(actualTos, MEALS_TO);
-                            }
-                        }
-                ));
+                .andExpect(model().attribute("meals", MEALS_TO));
     }
 
 }
